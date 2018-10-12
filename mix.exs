7413762420a3a99248618d05238b7c754ca064f3
Matt Doza <mdoza@me.com>
2018-10-11 20:25:40 -0700

    Added ex_docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule ElixirMath.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "ElixirMath",
+      source_url: "https://github.com/mdoza/elixir_math",
+      docs: [
+        main: "ElixirMath"
+      ]
     ]
   end
 
@@ -23,6 +30,7 @@ defmodule ElixirMath.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
